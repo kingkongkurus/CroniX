4d0a019af3e5a5fdd3530e997b443106de552bf1
Faizal <c007lc@gmail.com>
2014-05-09 17:29:15 +0800

    Sets app's root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 CAX::Application.routes.draw do
+  root 'application#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
