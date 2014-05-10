9e55e6f650c8181aed1f6a2c60b1d0cb463c6dbf
Faizal <c007lc@gmail.com>
2014-05-10 22:23:58 +0800

    Complete first-view

--- app/assets/javascripts/javascript.js
+++ app/assets/javascripts/javascript.js
@@ -1,7 +1,44 @@
 $(document).ready(function(){
 	$("#photos").click(function(){
-		$("#list_photos").slideToggle();
-		
+		$("#list_photos").slideToggle();	
+	});	
+});
+
+$(document).ready(function(){
+	$("#photos_1").click(function(){
+		$("#list_photos_1").slideToggle();	
+	});	
+});
+
+/* maps java
+$(document).ready(function(){
+      $('#world-map').vectorMap();
+		});
+}); */
+
+$(document).ready(function(){
+	$("#foods").click(function(){
+		$("#list_foods").slideToggle();
+	})
+})
+
+$(document).ready(function(){
+	$("#foods_1").click(function(){
+		$("#list_foods_1").slideToggle();
+	})
+})
+
+$(document).ready(function() {
+  $("#booking").on("mouseenter", function() {
+	// alert("1");
+	$('#pop_up').fadeIn('slow');
+    // $(this).addClass("highlight");
+	//$("#pop_up").animate({marginTop: "-20px"}, 5000);
+	// alert($(this).find("#pop_up").prop("id") + ", ");
+  });
+  $("#booking").on("mouseleave", function() {
+    //$(this).removeClass("highlight");
+    //$("#pop_up").animate({"top": "0","opacity": "0"}, "fast");
+  	$("#pop_up").fadeOut();
 	});
-	
 });
