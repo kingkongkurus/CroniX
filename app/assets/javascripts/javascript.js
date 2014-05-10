1d5566a6d6fc1d4eece1fb1c1e1a5ff8f6f99080
Faizal <c007lc@gmail.com>
2014-05-11 02:33:05 +0800

    video jquery

--- app/assets/javascripts/javascript.js
+++ app/assets/javascripts/javascript.js
@@ -1,3 +1,4 @@
+//js for listing photos
 $(document).ready(function(){
 	$("#photos").click(function(){
 		$("#list_photos").slideToggle();	
@@ -9,12 +10,6 @@ $(document).ready(function(){
 		$("#list_photos_1").slideToggle();	
 	});	
 });
-
-/* maps java
-$(document).ready(function(){
-      $('#world-map').vectorMap();
-		});
-}); */
 
 $(document).ready(function(){
 	$("#foods").click(function(){
@@ -28,6 +23,7 @@ $(document).ready(function(){
 	})
 })
 
+//js for booking button
 $(document).ready(function() {
   $("#booking").on("mouseenter", function() {
 	// alert("1");
@@ -43,13 +39,29 @@ $(document).ready(function() {
 	});
 });
 
+//js for group photo
 $(document).ready(function() {
   $("#group_name").on("click", function() {
 	$('#group').slideToggle('slow')
   });
 });
 
-
+//js for photo viewer
 $(document).ready(function(){
 	$(".fancybox").fancybox();
 });
+
+//js for promo video
+$(document).ready(function(){
+	$("#ad_video").on("click",function(){
+		$("#video").slideToggle();
+	})
+})
+
+
+
+/* maps java
+$(document).ready(function(){
+      $('#world-map').vectorMap();
+		});
+}); */
